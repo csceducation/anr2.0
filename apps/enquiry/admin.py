498491cec3e838c914220e11d7c2ce17556cab45
Senthil Kumaran <legendsenthilsk10@gmail.com>
2024-04-19 20:28:05 +0530

    Revert "initial commit"
    This reverts commit 02be2d3f9114ff26cad6d731f6f90448822e246c.

--- apps/enquiry/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from .models import Enquiry, Enquirylogs,StudentEnquiryModel
-
-admin.site.register(Enquiry)
-admin.site.register(Enquirylogs)
-admin.site.register(StudentEnquiryModel)
--- apps/revenue/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('today',views.today_income,name="today"),
-    path("month", views.month_income , name="month"),
-    path("revenue",views.all_income,name="all")
-]
